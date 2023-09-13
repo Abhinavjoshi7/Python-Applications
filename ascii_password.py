9599c33cd3e15de807ed03dee65f9535dff0665d
Abhinavjoshi7 <92068770+Abhinavjoshi7@users.noreply.github.com>
2023-09-12 19:53:00 -0600

    solved password decode question

--- ascii_password.py
+++ ascii_password.py
@@ -1,5 +1,5 @@
 #ascii values are in reverse order
-s = input("Enter a string: ") #01 31 15 05 94 101 411 101 401 611 111 801 801 101 401
+s = input("Enter a string: ") #0131150594101411101401611111801801101401
 #reverse the string  
 s = s[::-1] # 104 101 108 108 111 116 104 101 114 101 49 50 51 13 10
 print(s)
@@ -10,6 +10,11 @@ while(i<len(s)-1):
     if x=="32": #32 in ascii is a space
         result = result + " " #add as a result
     elif int(x) in range (65,91) or int(x) in range(97,100):
-        res = res + chr(int(x)) #chr converts the numeric value into its representing ascii value 
+        result = result + chr(int(x)) #chr converts the numeric value into its representing ascii value 
+    elif i+2<len(s): #for 3 length characters
+        x = x+s[i+2] #add two digits + one digit, two digits come from line line 9
+        result = result + chr(int(x))
+        i+=1
         
-        
+    i+=2 #second iteration, i will become two in line 6 and keep adding
+print(result)  
