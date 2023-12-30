d4d4dfdeed8da325f6f0054b04f48ecfde55a582
Abhinavjoshi7 <92068770+Abhinavjoshi7@users.noreply.github.com>
2023-12-30 12:37:06 -0700

    Comments for self help

--- book.py
+++ book.py
@@ -11,6 +11,8 @@ class TextBook(Book):
     def __init__(self, title, author, edition):
         super().__init__(title, author) # this will run  self.title = title and self.author = author. Calling the base class
         self.edition = edition
+ 
+
 book = Book(title='All the light', author='Some person')
 textbook = TextBook(title='Maths', author='RD Sharma', edition=2)
 print(book)
